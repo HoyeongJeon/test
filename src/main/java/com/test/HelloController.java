e30e5952d73460b97e0054122f6405c112b0de99
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-13 19:55:56 +0900

    cicd 수정

--- src/main/java/com/test/HelloController.java
+++ src/main/java/com/test/HelloController.java
@@ -1,13 +1,13 @@
 package com.test;
 
-import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping
     public String hello() {
-        return "Hello, world!!!";
+        return "Hello, world!";
     }
 }
