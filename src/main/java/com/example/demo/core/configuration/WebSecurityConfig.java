77cda94e50027420d8109092f30976a45a8f1138
HoyeongJeon <hoyeong0302@gmail.com>
2024-10-13 21:38:46 +0900

    first commit

--- src/main/java/com/example/demo/core/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/demo/core/configuration/WebSecurityConfig.java
@@ -1,12 +1,14 @@
 package com.example.demo.core.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
 @EnableWebSecurity
+@Configuration
 @Component
 public class WebSecurityConfig {
 
